4449f65f756337200d52c06eeb0bf6b49a980853
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-03-05 21:22:56 +0100

    Added a comment to day 1 code

--- day_1.py
+++ day_1.py
@@ -1,3 +1,5 @@
+# Day 2, generates a person's name and a pet's name
+
 print("Welcome to the Band name Generator.")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's the name of your pet?\n")
